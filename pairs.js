49c5e1d1b08c0749d50c0214c82814e968065905
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-03-24 10:34:05 +0100

    Solution using reduce()

--- pairs.js
+++ pairs.js
@@ -1,1 +1,24 @@
 const pairs = [90, 10, 10, 1, 90, 90, 90, 10, 22];
+
+function findPairs(arr) {
+  let numbers = arr.reduce((obj, item) => {
+    if (!obj[item]) {
+      obj[item] = 0;
+    }
+    return obj;
+  }, {});
+
+  arr.forEach((x) => {
+    numbers[x] += 1;
+  });
+
+  for (const number in numbers) {
+    numbers[number] = Math.floor(numbers[number] / 2);
+  }
+
+  let myArr = Object.keys(numbers);
+
+  return myArr.map((x) => ({ [x]: numbers[x] }));
+}
+
+console.log(findPairs(pairs));
